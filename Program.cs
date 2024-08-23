58ebe5177032b15a40cc426f00123d4d241337c4
Diocleciano Gonçalves de Oliveira <goncalves@intruser.com.br>
2024-08-23 07:54:12 -0300

    Primeiro commit do Curso de CSharp da Cod3r pela Udemy

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(String[] args) {
+            Console.WriteLine("Hello, World!");
+            {
+                Console.WriteLine("Hello, World2!");
+            }
+        }
+    }
+}
