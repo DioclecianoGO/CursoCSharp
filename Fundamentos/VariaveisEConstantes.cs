6a47b0e2e997fde24eb50d4dc3ef7c5d6274a652
Diocleciano Gonçalves de Oliveira <goncalves@intruser.com.br>
2024-08-23 11:53:34 -0300

    Atualização - Fundamentos: Variáveis e Constantes

---
+++ Fundamentos/VariaveisEConstantes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    internal class VariaveisEConstantes {
+        public static void Executar() {
+            // area da circunferencia
+            double raio = 4.5;
+
+        }
+    }
+}
