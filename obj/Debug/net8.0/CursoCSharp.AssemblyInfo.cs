afc7b70a77e903cc382e14900da0740dd0a1b29c
Diocleciano Gonçalves de Oliveira <goncalves@intruser.com.br>
2024-08-26 16:26:27 -0300

    Atualização - Fundamentos: Operadores de Unários

--- obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9218eb3cc263f46e179b5503d40de8a7436a08b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d06f6c2ba1b8a0fadf60d62ad0c98c74d2fb7eb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
