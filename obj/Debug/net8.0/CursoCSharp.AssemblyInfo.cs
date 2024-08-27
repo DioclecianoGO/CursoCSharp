b8a7f6c72440b700c7de30da350f65b8e759d74c
Diocleciano Gonçalves de Oliveira <goncalves@intruser.com.br>
2024-08-27 11:46:38 -0300

    Atualização - Estruturas de Controle: Estrutura Switch

--- obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5905a7bbba5a724bd4f025f30f94dd42196eb630")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e31b0d9be0cb2db2e702c78ad98291c64d7b4e93")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
